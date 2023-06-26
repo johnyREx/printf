eba2e61a7d2e14b5dc7cff784c349d4e67baea2b
johnyREx <johnkwofie99@icloud.com>
2023-06-26 03:24:26 -0700

    Task 0

--- main.h
+++ main.h
@@ -1,5 +1,30 @@
 #ifndef MAIN_H
 #define MAIN_H
 
+#include <stdarg.h>
+
+/**
+ * struct specifier - struct for formst specifiers
+ *
+ * @spec: the format specifier
+ * @f: The function associated with the format specifier
+ */
+typedef struct specifier
+{
+	char *spec;
+	int (*f)(va_list);
+} spec_t;
+
 int _printf(const char *format, ...);
+int print_c(va_list arg);
+int print_s(va_list arg);
+int print_d(va_list arg);
+int print_i(va_list arg);
+int print_percent(va_list arg);
+int print_b(va_list arg);
+int print_u(va_list arg);
+int print_o(va_list arg);
+int print_x(va_list arg);
+int print_X(va_list arg);
+
 #endif
