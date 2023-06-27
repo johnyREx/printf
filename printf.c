87abce0a5cd5c3d4ee38466cf0b58207b5208d1e
johnyREx <johnkwofie99@icloud.com>
2023-06-27 10:04:53 -0700

    task 0 errors

---
+++ printf.c
@@ -0,0 +1,68 @@
+#include "main.h"
+#include <stdio.h>
+
+/**
+ * _printf - produces output according to a format string.
+ * @format: The format string
+ *
+ * Return The number of characters printed.
+ */
+
+int _printf(const char *format, ...)
+{
+	va_list args;
+	int printed_chars = 0;
+	char c;
+	char *str;
+
+	va_start(args, format);
+
+	while (*format)
+	{
+		if (*format == '%')
+		{
+			format++;
+
+			switch (*format)
+			{
+				case 'c':
+					c = va_arg(args, int);
+					write(1, &c, 1);
+					printed_chars++;
+					break;
+				case 's':
+					str = va_arg(args, char *);
+					if (str == NULL)
+						str = "(null)";
+					while (*str)
+					{
+						write(1, str, 1);
+						str++;
+						printed_chars++;
+					}
+					break;
+				case '%':
+					write(1, "%", 1);
+					printed_chars++;
+					break;
+				default:
+					write(1, "%", 1);
+					write(1, &(*format), 1);
+					printed_chars += 2;
+					break;
+			}
+		}
+
+		else
+		{
+			write(1, &(*format), 1);
+			printed_chars++;
+		}
+
+		format++;
+	}
+
+	va_end(args);
+
+	return (printed_chars);
+}
