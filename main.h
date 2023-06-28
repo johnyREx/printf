97c6a1a7be35fe936e29ac3628b90db5e60d9def
johnyREx <johnkwofie99@icloud.com>
2023-06-28 07:13:47 -0700

    second chance to get all right

--- main.h
+++ main.h
@@ -23,12 +23,22 @@ int _putchar(char c);
 int _printf(const char *format, ...);
 void _vprintf(const char *format, va_list args);
 int parser(const char *format, conver_t funct_list[], va_list args);
-int p_char(va_list);
+int p_char(va_list args);
 int p_string(va_list args);
-int p_percent(va_list);
+int p_percent(va_list args);
+int unsigned_number(va_list args);
+int print_number(va_list args);
+int p_binary(va_list args);
+int p_reversed(va_list args);
+int rot13(va_list args);
 int print_unsigned_number(unsigned int n);
-int print_number(va_list);
-int p_integer(va_list);
+int print_integer(va_list args);
+int p_octal(va_list args);
+int p_hex(va_list args);
+int p_heX(va_list args);
+unsigned int base_len(unsigned int, int);
+char *rev_string(char *);
+void write_base(char *str);
+char *_memcpy(char *dest, char *src, unsigned int n);
 
 #endif
-
